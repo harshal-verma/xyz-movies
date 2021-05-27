cd47ce26ad6da4084b924fbf3075b879b0ccd0cf
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-05-27 20:48:13 +0530

    updated main display

--- app.js
+++ app.js
@@ -4,26 +4,39 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+const main = document.querySelector('main');
+
 async function getMovies(){
     const response = await fetch(APIURL);
     const respData = await response.json();
     console.log(respData);
      
     respData.results.forEach((movie) => {
-        const div = document.createElement('div');
-        // img.src = IMGPATH + movie.poster_path;
-        div.innerHTML = `<div class="movie">
-        <img src="${IMGPATH + movie.poster_path}" alt="movie poster">
+        const movieEl = document.createElement('div');
+
+        movieEl.innerHTML = `<div class="movie">
+        <img src="${IMGPATH + movie.poster_path}" 
+        alt="${movie.title}">
         <div class="movie-info">
-            <h3>Movie title</h3>
-            <span>8.4</span>
+            <h3>${movie.title}</h3>
+            <span class=${getClassByRate(movie.vote_average)}>${movie.vote_average}</span>
         </div>
     </div>`
 
-        document.body.appendChild(div)
+        main.appendChild(movieEl)
     })
 
     return respData;
+}
+
+function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green';
+    }else if(vote >= 5){
+        return 'orange';
+    }else{
+        return 'red';
+    }
 }
 
 getMovies();
