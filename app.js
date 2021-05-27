0c98782e1a3ab01505b837b1de1825d65c152a92
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-05-27 17:07:47 +0530

    updated app.js

--- app.js
+++ app.js
@@ -3,3 +3,19 @@ const APIURL =
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+async function getMovies(){
+    const response = await fetch(APIURL);
+    const respData = await response.json();
+    
+    respData.results.forEach((movie) => {
+        const img = document.createElement('img');
+        img.src = IMGPATH + movie.poster_path;
+
+        document.body.appendChild(img);
+    });
+
+    return respData;
+}
+
+getMovies();
