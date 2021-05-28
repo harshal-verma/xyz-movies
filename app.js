e9d37dedca4195669b6fd5ef674fb921c4633fd0
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-05-28 10:46:10 +0530

    feat search bar

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const main = document.querySelector('main');
+const form = document.querySelector('form');
+const search = document.getElementById("search");
 
 async function getMovies(){
     const response = await fetch(APIURL);
     const respData = await response.json();
-    console.log(respData);
-     
     respData.results.forEach((movie) => {
         const movieEl = document.createElement('div');
 
