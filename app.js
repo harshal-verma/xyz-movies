09c51d2d9168e5975770334719a88b2be5e1e17b
harshal-verma <81582281+harshal-verma@users.noreply.github.com>
2021-06-14 20:54:21 +0530

    updated app.js

--- app.js
+++ app.js
@@ -9,35 +9,49 @@ const form = document.querySelector('form');
 const search = document.getElementById("search");
 const movieCard = document.querySelector(".movie");
 
-// initially get fav movies
 getMovies(APIURL);
 
 async function getMovies(url){
     const response = await fetch(url);
     const respData = await response.json();
       showMovies(respData.results);
-    //   console.log(respData.results);
-    return respData.results;
 }
 
 function showMovies(movies){
+    const arrLength = movies.length;
     main.innerHTML = "";
-    movies.forEach((movie) => {
+    for(let i = 0 ; i< arrLength ; i++){
         const movieEl = document.createElement('div');
-
         movieEl.innerHTML = `
         <div class="movie" onclick="viewMovieCard(event)">
-        <img src="${IMGPATH + movie.poster_path}" 
-        alt="${movie.title}">
+        <img id=${i} src="${IMGPATH + movies[i].poster_path}" 
+        alt="${movies[i].title}">
         <div class="movie-info">
-            <h3>${movie.title}</h3>
-            <span class=${getClassByRate(movie.vote_average)}>${movie.vote_average}</span>
+            <h3>${movies[i].title}</h3>
+            <span class=${getClassByRate(movies[i].vote_average)}>${movies[i].vote_average}</span>
             </div>
         </div>
-        `
+        `  
         main.appendChild(movieEl)
-    })
+    }
 }
+
+async function viewMovieCard(e){
+    const response = await fetch(APIURL);
+    const respData = await response.json();
+    let movies = respData.results;
+    console.log(movies);
+    console.log(e);
+    console.log(e.target.title)
+    main.innerHTML = " ";
+    const singleMovieEl = document.createElement("div");
+    singleMovieEl.innerHTML = `
+    <img src="${IMGPATH + movies[e.target.id].poster_path}" 
+    alt="${movies[e.target.id].title}">
+    `
+    main.appendChild(singleMovieEl);
+}
+
 
 function getClassByRate(vote){
     if(vote >= 8){
